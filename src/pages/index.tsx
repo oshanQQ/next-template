7cc3c311b8b1868a08758f3f70312c863174fedc
take-cantik <take.cantik17@gmail.com>
2022-02-10 20:33:40 +0900

    Fix: rebase base

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 import type { NextPage } from "next";
-import HomeTemplate from "~/component/template/Index";
+import { Fragment } from "react";
+import { useAuth } from "~/hook/useAuth";
 
 const Home: NextPage = (): JSX.Element => {
-  return <HomeTemplate />;
+  const { auth, login, logout } = useAuth();
+  console.log(auth.currentUser);
+
+  return (
+    <Fragment>
+      <h1>{auth.currentUser ? auth.currentUser.displayName : "ahiahi"}</h1>
+      <button onClick={login}>login</button>
+      <button onClick={logout}>logout</button>
+    </Fragment>
+  );
 };
 
 export default Home;
