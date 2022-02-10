29d5d441934bd268f17a248a43feec5734a0420a
take-cantik <take.cantik17@gmail.com>
2022-02-10 20:30:27 +0900

    Add: Header template

--- src/component/template/Index/index.tsx
+++ src/component/template/Index/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from "next";
+import { Fragment } from "react";
+import { Header } from "~/component/shared/Header";
 
 const HomeTemplate: NextPage = (): JSX.Element => {
-  return <h1>Hello, world!</h1>;
+  return (
+    <Fragment>
+      <Header />
+      <h1>Hello, world!</h1>
+    </Fragment>
+  );
 };
 
 export default HomeTemplate;
