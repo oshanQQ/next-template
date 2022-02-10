0edf10713793802aabcae23d053b29d97ea905e6
take-cantik <take.cantik17@gmail.com>
2022-02-10 20:48:35 +0900

    Update: use at index template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,8 @@
 import type { NextPage } from "next";
-import { Fragment } from "react";
-import { useAuth } from "~/hook/useAuth";
+import HomeTemplate from "~/component/template/Index";
 
 const Home: NextPage = (): JSX.Element => {
-  const { auth, login, logout } = useAuth();
-  console.log(auth.currentUser);
-
-  return (
-    <Fragment>
-      <h1>{auth.currentUser ? auth.currentUser.displayName : "ahiahi"}</h1>
-      <button onClick={login}>login</button>
-      <button onClick={logout}>logout</button>
-    </Fragment>
-  );
+  return <HomeTemplate />;
 };
 
 export default Home;
