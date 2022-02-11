033affde5e8021335218c663df514c2a6f5bcebe
take-cantik <take.cantik17@gmail.com>
2022-02-11 23:42:11 +0900

    Add: useText hook

---
+++ src/hook/useText.ts
@@ -0,0 +1,22 @@
+import { User } from "firebase/auth";
+import { addDoc, collection } from "firebase/firestore";
+import { firestore } from "~/infra/firebase";
+
+export const useText = () => {
+  const addText = async (user: User, text: string) => {
+    try {
+      const docRef = await addDoc(collection(firestore, "texts"), {
+        uid: user.uid,
+        text: text
+      });
+
+      console.log(docRef);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return {
+    addText
+  };
+};
