69630a461545ee388c8437ec94701651200c8756
take-cantik <take.cantik17@gmail.com>
2022-02-11 23:41:21 +0900

    Add: firestore

--- src/infra/firebase.ts
+++ src/infra/firebase.ts
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { firebaseConfig } from "~/constant";
 
 export const firebase = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebase);
+export const firestore = getFirestore(firebase);
