07466ccd9054bba89bd2a4696c771c1f0a43a01e
take-cantik <take.cantik17@gmail.com>
2022-02-11 23:42:39 +0900

    Update: use firestore

--- src/component/template/Firebase/index.tsx
+++ src/component/template/Firebase/index.tsx
@@ -1,9 +1,20 @@
-import { Fragment } from "react";
+import { ComponentPropsWithRef, Fragment } from "react";
 import { Container } from "~/component/layout/Container";
 import { Header } from "~/component/shared/Header";
 import { useAuth } from "~/hook/useAuth";
+import { useText } from "~/hook/useText";
 
-export const FirebaseTemplate = () => {
+export interface FirebaseTemplateProps extends ComponentPropsWithRef<"div"> {
+  text: string;
+  onChangeText: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  submit: () => void;
+}
+
+export const FirebaseTemplate = ({
+  text,
+  onChangeText,
+  submit
+}: FirebaseTemplateProps) => {
   const { auth, login, logout } = useAuth();
   console.log(auth.currentUser);
 
@@ -15,6 +26,12 @@ export const FirebaseTemplate = () => {
           <h1>{auth.currentUser ? auth.currentUser.displayName : "ahiahi"}</h1>
           <button onClick={login}>login</button>
           <button onClick={logout}>logout</button>
+          {auth.currentUser && (
+            <div>
+              <input type={"text"} value={text} onChange={onChangeText} />
+              <button onClick={submit}>add text</button>
+            </div>
+          )}
         </Container>
       </main>
     </Fragment>
