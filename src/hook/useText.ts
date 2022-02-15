296b039d639424d5013343888e102b6605fcb109
take-cantik <take.cantik17@gmail.com>
2022-02-15 23:32:08 +0900

    Fix: firebaseConfig

--- src/hook/useText.ts
+++ src/hook/useText.ts
@@ -1,11 +1,13 @@
 import { User } from "firebase/auth";
-import { addDoc, collection, doc, getDoc, setDoc } from "firebase/firestore";
+import { addDoc, collection } from "firebase/firestore";
+import { firebaseConfig } from "~/constant";
 import { firestore } from "~/infra/firebase";
 
 export const useText = () => {
   const addText = async (user: User, text: string) => {
     try {
       console.log("大丈夫そ？");
+      console.log(firebaseConfig);
       const docRef = await addDoc(collection(firestore, "texts"), {
         uid: user.uid,
         text: text
