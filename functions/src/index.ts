1490284063ec2a94b6cb64d42bb7914305fdc154
take-cantik <take.cantik17@gmail.com>
2022-02-17 23:24:09 +0900

    Test: use firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,18 @@
-import * as functions from "firebase-functions";
+import { config, logger, https } from "firebase-functions";
+import { initializeApp, firestore } from "firebase-admin";
+
+initializeApp(config().firebase);
+const db = firestore();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
+export const helloWorld = https.onRequest((request, response) => {
+  logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
+
+// export const getTexts = https.onRequest((request, response) => {
+//   const res = db.collection("texts").get();
+//   console.log(res);
+// });
