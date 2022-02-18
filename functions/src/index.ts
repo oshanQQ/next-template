32c3e6c3be5e2769e726c7a522b2ef44f4f59dd7
take-cantik <take.cantik17@gmail.com>
2022-02-18 15:51:26 +0900

    Update: set suchedule func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import { logger, https } from "firebase-functions";
+import { logger, https, pubsub, EventContext } from "firebase-functions";
 import { initializeApp, firestore, credential } from "firebase-admin";
 
 const app = initializeApp({
@@ -10,16 +10,27 @@ const db = firestore(app);
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = https.onRequest((request, response) => {
-  logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+// export const helloWorld = https.onRequest((request, response) => {
+//   logger.info("Hello logs!", { structuredData: true });
+//   response.send("Hello from Firebase!");
+// });
 
-export const getTexts = https.onRequest(async (request, response) => {
-  const texts: Text[] = [];
-  const res = await db.collection("texts").get();
-  res.forEach((doc) => {
-    texts.push(doc.data() as Text);
+// export const getTexts = https.onRequest(async (request, response) => {
+//   const texts: Text[] = [];
+//   const res = await db.collection("texts").get();
+//   res.forEach((doc) => {
+//     texts.push(doc.data() as Text);
+//   });
+//   response.send(texts);
+// });
+
+export const scheduledTest = pubsub
+  .schedule("every 3 minutes")
+  .onRun(async (context: EventContext) => {
+    await db.collection("texts").add({
+      uid: "test",
+      text: "ahiahi"
+    });
+
+    return null;
   });
-  response.send(texts);
-});
